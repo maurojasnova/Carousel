d7421442bac73c203fad7cfd35305e081dc5ea08
maurojasnova <andrsnov@gmail.com>
2023-04-23 10:22:03 -0500

    Fixed error with footer positioning

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,21 @@
-import '../../styles/globals.css';
-import { Header } from './components/Header'
-import Footer from './components/Footer';
+import "../../styles/globals.css";
+import { Header } from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata = {
   title: "Street Burguer",
   description: "Comida rápida",
 };
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header />
-        {children}
-        <Footer />
+        <div className="page-container">
+          <Header />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
